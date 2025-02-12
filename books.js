4f831c532bcf7c39a4907cc8f860c5e55789478a
cyp-vet <lrx96121@gmail.com>
2025-02-13 01:46:22 +0800

    Add files via upload

---
+++ books.js
@@ -0,0 +1,34 @@
+const books = [
+  {
+    "image": "2001.png",
+    "number": "2001",
+    "category": " 临床医学",
+    "title": "小动物内科学第六版 上中",
+    "date": "2019",
+    "baiduLink": "23动物内科学第六版 （下）.pdf 链接: https://pan.baidu.com/s/1ccCZvyVAoqIX3xp6Zj-h9A?pwd=8645 提取码: 8645",
+  },
+  {
+    "image": "2001.png",
+    "number": "2002",
+    "category": "兽医外科学",
+    "title": "现代兽医外科学",
+    "date": "2012",
+    "baiduLink": "小动物内科学第六版 （下）.pdf 链接: https://pan.baidu.com/s/1ccCZvyVAoqIX3xp6Zj-h9A?pwd=8645 提取码: 8645",
+  },
+  {
+    "image": "3.png",
+    "number": "2003",
+    "category": "兽医3科学",
+    "title": "现代兽医外科学",
+    "date": "2012",
+    "baiduLink": "1动物内科学第六版 （下）.pdf 链接: https://pan.baidu.com/s/1ccCZvyVAoqIX3xp6Zj-h9A?pwd=8645 提取码: 8645",
+  },
+  {
+    "image": "4.png",
+    "number": "2004",
+    "category": "兽医3科学",
+    "title": "现代兽医外科学",
+    "date": "2012",
+    "baiduLink": "小动物内科学第六版 （下）.pdf 链接: https://pan.baidu.com/s/1ccCZvyVAoqIX3xp6Zj-h9A?pwd=8645 提取码: 8645",
+  }
+];
