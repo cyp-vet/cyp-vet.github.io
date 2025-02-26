b6910ce107eb7d917504ec2abbabde4c8d157f49
cyp-vet <lrx96121@gmail.com>
2025-02-27 02:05:31 +0800

    Add files via upload

--- books.js
+++ books.js
@@ -6,9 +6,7 @@ const books = [
     "title": "小動物內科學第六版 上中下全冊",
     "date": "2019",
     "language": "繁体中文",
-    "baiduLink": "通过网盘分享的文件：Preview\n" +
-        "链接: https://pan.baidu.com/s/1pb61AC4ZjyH9b2XdKLLj6g?pwd=1d9v 提取码: 1d9v \n" +
-        "--来自百度网盘",
+    "baiduLink": "通过网盘分享的文件：Full version 链接: https://pan.baidu.com/s/1z1JgBxnngDHxhNchjkv2gg?pwd=umyh 提取码: umyh--来自百度网盘",
   },
   {
     "image": "1",
