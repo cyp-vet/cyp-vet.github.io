92cf9898e7517ce8bf86eaf454bf3d65f2a8f3b6
cyp-vet <lrx96121@gmail.com>
2025-02-27 02:28:18 +0800

    Add files via upload

--- books.js
+++ books.js
@@ -4,18 +4,18 @@ const books = [
     "number": "2001",
     "category": "临床兽医学",
     "title": "小動物內科學第六版 上中下全冊",
-    "date": "2019",
+    "date": "2022",
     "language": "繁体中文",
-    "baiduLink": "通过网盘分享的文件：Full version 链接: https://pan.baidu.com/s/1z1JgBxnngDHxhNchjkv2gg?pwd=umyh 提取码: umyh--来自百度网盘",
+    "baiduLink": "通过网盘分享的文件：Preview 链接: https://pan.baidu.com/s/1hJB59w83rtPa37WMI8HLrw?pwd=tdcb 提取码: tdcb",
   },
   {
-    "image": "1",
-    "number": "",
-    "category": "",
-    "title": "",
-    "date": "",
-    "language": "",
-    "baiduLink": "",
+    "image": "2002.png",
+    "number": "2002",
+    "category": "临床兽医学",
+    "title": "Small Animal Internal Medicine, Sixth Edition",
+    "date": "2020",
+    "language": "English",
+    "baiduLink": "通过网盘分享的文件：Preview 链接: https://pan.baidu.com/s/1OFiWJ_6iI6eOqllkML7gWQ?pwd=qt6i 提取码: qt6i",
   }
 
   ]
