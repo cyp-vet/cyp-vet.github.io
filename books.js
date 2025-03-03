35e5ce49b9dafdb297442758dcadef9933bc3dc2
cyp-vet <lrx96121@gmail.com>
2025-03-03 10:14:34 +0800

    Add files via upload

--- books.js
+++ books.js
@@ -21,10 +21,19 @@ const books = [
     "image": "3001.png",
     "number": "3001",
     "category": "兽医诊断与实验技术",
-    "title": "犬猫临床心脏超音波 - 翻译版",
+    "title": "犬猫临床心脏超音波 Clinical Echocardiography of the Dog and Cat-中文翻译版",
     "date": "2025",
     "language": "简体中文",
     "baiduLink": "通过网盘分享的文件：Preview 链接: https://pan.baidu.com/s/180Yv2aM4DbWZuv6uK9fztA?pwd=9xkj 提取码: 9xkj",
+  },
+  {
+    "image": "2003.png",
+    "number": "2003",
+    "category": "临床兽医学",
+    "title": "宠物血液生化分析",
+    "date": "2019",
+    "language": "简体中文",
+    "baiduLink": "通过网盘分享的文件：Preview 链接: https://pan.baidu.com/s/1WfMhbc82cm55JWU-VLymow?pwd=fgmj 提取码: fgmj",
   }
 
   ]
